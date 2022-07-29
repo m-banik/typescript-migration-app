b3bac74de3e47772a453e671341e78ac40e9e4b4
m-banik <mrn.banik@gmail.com>
2022-07-29 02:22:29 +0200

    Implement the movies list related components refactor

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,7 @@
 export * from './App';
+export * from './button';
+export * from './editable-movie';
+export * from './movie';
+export * from './movie-button-panel';
 export * from './movies-list';
 export * from './movies-list-element';
--- src/components/movies-list-element/index.jsx
+++ src/components/movies-list-element/index.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectMovieById } from '../../models';
+import { useSelector, useDispatch } from 'react-redux';
+import { EditableMovie, Movie, MovieButtonPanel } from '..';
+import { selectMovieById, deleteMovieStart } from '../../models';
 import classes from './styles.module.less';
 
 export const MoviesListElement = ({ movieId, elementIndex }) => {
   const movie = useSelector((store) => selectMovieById(movieId)(store));
+  const dispatch = useDispatch();
+
+  const [isMovieEdited, setIsMovieEdited] = React.useState(false);
+
+  const handleDeleteMovie = React.useCallback(
+    () => dispatch(deleteMovieStart(movieId)),
+    [movieId, dispatch]
+  );
+
+  const handleSetMovieEditionMode = React.useCallback(
+    () => setIsMovieEdited(true),
+    [setIsMovieEdited]
+  );
+
+  const handleUnsetMovieEditionMode = React.useCallback(
+    () => setIsMovieEdited(false),
+    [setIsMovieEdited]
+  );
 
   return (
     <li className={classes.moviesListElement}>
-      <p className={classes.content}>
-        {elementIndex === undefined ? null : (
-          <span className={classes.index}>{elementIndex + 1}</span>
-        )}
-        <span className={classes.title}>{movie.title}</span>
-        <span className={classes.premiere}>({movie.premiere_date})</span>-{' '}
-        <span>{movie.director}</span>
-      </p>
+      {isMovieEdited ? (
+        <EditableMovie
+          elementIndex={elementIndex}
+          title={movie.title}
+          premiereDate={movie.premiere_date}
+          director={movie.director}
+        />
+      ) : (
+        <Movie
+          elementIndex={elementIndex}
+          title={movie.title}
+          premiereDate={movie.premiere_date}
+          director={movie.director}
+        />
+      )}
+      <MovieButtonPanel
+        isMovieEdited={isMovieEdited}
+        onUnsetMovieEditionMode={handleUnsetMovieEditionMode}
+        onSetMovieEditionMode={handleSetMovieEditionMode}
+        onDeleteMovie={handleDeleteMovie}
+      />
     </li>
   );
 };
