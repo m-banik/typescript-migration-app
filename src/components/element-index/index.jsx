bd8c5a33165dd3a6fc976356748b8d0e0c34efb3
m-banik <mrn.banik@gmail.com>
2022-07-29 22:11:18 +0200

    Do a slight code refactor

--- src/components/element-index/index.jsx
+++ src/components/element-index/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import classes from './styles.module.less';
 
 export const ElementIndex = ({ index }) => {
-  const indexElementClasses = [
-    classes.index,
+  const classNames = [
+    classes.elementIndex,
     index >= 9 ? classes.greater : ''
   ].join(' ');
 
-  return <span className={indexElementClasses}>{index + 1}</span>;
+  return <span className={classNames}>{index + 1}</span>;
 };
